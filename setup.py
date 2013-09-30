905c3dd522a66a83255f5769b1d68aa9fd82320f
Joe Z <jzarate@gmail.com>
2013-09-30 16:37:38 -0400

    pypi enhancements : rst README and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.txt') as file:
+    long_description = file.read()
+
+
+setup(name='PyDoof', version='1.0', author='JoeZ99',
+      author_email='jzarate@gmail.com',
+      description="Doofinder's search & management API client",
+      url='https://github.com/doofinder/pydoof',
+      packages=['pydoof'],
+      requires=['requests(>=1.2.3)'],
+      install_requires=['requests>=1.2.3'],
+      provides=['PyDoof'],
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
+          ]
+      )
