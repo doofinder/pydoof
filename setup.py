051d8afe32fdd0b86f2d0eefab92752ae2167fe6
Joe Z <jzarate@gmail.com>
2016-07-15 14:57:12 -0400

    new release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.txt') as file:
     long_description = file.read()
 
 
-setup(name='PyDoof', version='2.4.0', author='JoeZ99',
+setup(name='PyDoof', version='2.4.1', author='JoeZ99',
       author_email='jzarate@gmail.com',
       description="Doofinder's search & management API client",
       url='https://github.com/doofinder/pydoof',
