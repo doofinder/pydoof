0e3b934e486d0421e2b578c80d4ed930b72712b8
Joe Z <jzarate@gmail.com>
2016-11-07 10:49:10 -0500

    make tests: rearrangements in file tree

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ with open('README.txt') as file:
     long_description = file.read()
 
 
-setup(name='PyDoof', version='2.4.5', author='JoeZ99',
+setup(name='PyDoof', version='2.5.0', author='JoeZ99',
       author_email='jzarate@gmail.com',
       description="Doofinder's search & management API client",
       url='https://github.com/doofinder/pydoof',
       packages=['pydoof'],
-      requires=['requests(>=1.2.3)', 'HTTPretty>=0.0.14'],
+      requires=['requests(>=1.2.3)'],
       install_requires=['requests>=1.2.3'],
+      test_suite='nose.collector',
+      tests_require=['nose', 'HTTPretty>=0.0.14'],
       provides=['PyDoof'],
       long_description=long_description,
       classifiers=[
