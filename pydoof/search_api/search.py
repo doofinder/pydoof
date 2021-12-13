0c2ea67edbb70af898309cc67f053a8c6c18d64b
Adrián <1659540+mrroot5@users.noreply.github.com>
2021-12-13 18:13:37 +0100

    Bk 721 create suggestions request (#21)
    * Changed package version to 4.
    * Set development status to level 5: production / stable.
    * Added Search API v6 suggest request.
    * Upgraded Python requirements to version to 3.6.7.

--- pydoof/search_api/search.py
+++ pydoof/search_api/search.py
@@ -1,4 +1,5 @@
 from enum import Enum, unique
+from typing import Any, Dict, List
 
 from pydoof.search_api.api_client import SearchAPIClient
 from pydoof.helpers import parse_query_params
@@ -18,9 +19,9 @@ class Transformers(Enum):
     ONLY_IDS = "onlyid"
 
 
-def query(hashid, query, filter_=None, exclude=None, index_name=None,
-          query_name=None, sort=None, page=None, rpp=None, transformer=None,
-          no_stats=None, **opts):
+def query(hashid: str, query: str, filter_: Dict[str, Any]=None, exclude: Dict[str, Any]=None,
+          index_name: str=None, query_name: str=None, sort: List[Dict[str, str]]=None,
+          page: int=None, rpp: int=None, transformer: str=None, no_stats: bool=None, **opts):
     """
     Queries items indexed in a search engine.
 
@@ -81,54 +82,30 @@ def query(hashid, query, filter_=None, exclude=None, index_name=None,
     )
 
 
-def suggest(hashid, query, filter_=None, exclude=None, sort=None, page=None,
-            rpp=None, transformer=None, no_stats=None, **opts):
+def suggest(hashid: str, query: str, indices: List[str]=None, stats: bool=None, session_id: str=None, **opts):
     """
-    Fetchs suggestions for terms based on the items indexed in a search engine.
+    Fetch suggestions for terms based on the items indexed in a search engine.
 
     Args:
         hashid (str): Unique search engine id. Indicates to which search engine
             we are doing the query.
         query (str): The terms we are looking for in the items of the search
             engine.
-        filter_ (dict, optional): A dictionary that indicates a filter for
-            items. For instance, look for those items of color "blue". Default
-            to None.
-        exclude (dict, optional): A dictionary that indicates an exclude rule
-            for items. For instance, exclude those items that belong to `Foo`
-            category. Default to None
-        sort (lst, optional): Indicates a sorting rule for results. If
-            sort is a list of strings, each element will define a field to sort
-            by in ascending order. If sort is a list of dictionaries, you can
-            set the order. For instance, sort: [{'color': 'desc'}] will sort
-            results by color name in descending order. If you do not provided
-            one, results will be ordered by score in descending order. Default
-            to None.
-        page (int, optional): Indicates a page of results. If you provide a
-            page, `suggest` will return the results from that page. Default to
-            None.
-        rpp (int, optional): Indicates how many results to fetch by page,
-            minimum 1, maximum 100. Default to 10.
-        transformer (str, optional): Indicates a transformation to apply to
-            items in result. It could be one of "basic" or "onlyid". If none is
-            set, items will not be transformed. Default to None.
-        no_stats (bool, optional): Indicates if the query should be recorded
-            in search stats. If it is true, it will not be recorded. Default to
-            False.
+        indices (List[str], optional): With the indices parameter you can specify to search within one specific Index.
+            If this parameter is not provided, the search will work with all Indices.
+            Note that [ and ] characters should be escaped (%5B and %5D) in all cases.
+            Example: indices[]=product&indices[]=page; indices=products
+        stats (boolean, optional): Enable/Disable this search in stats reports.
+        session_id (str, optional, <= 32 characters): The current session ID, must be unique for each user.
     """
     query_params = parse_query_params({
-        'hashid': hashid,
         'query': query,
-        'filter': filter_,
-        'exclude': exclude,
-        'sort': sort,
-        'page': page,
-        'rpp': rpp,
-        'transformer': transformer,
-        'nostats': no_stats
+        'indices': indices,
+        'stats': stats,
+        'session_id': session_id
     })
     api_client = SearchAPIClient(**opts)
     return api_client.get(
-        '/5/suggest',
+        f'/6/{hashid}/_suggest',
         query_params=query_params
     )
--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ with open('README.md', 'r', encoding='utf-8') as file:
     long_description = file.read()
 
 
-setup(name='PyDoof', version='3.3.5', author='Doofinder',
+setup(name='PyDoof', version='4.0.0', author='Doofinder',
+      python_requires='>=3.6.7',
       author_email='support@doofinder.com',
       description="Doofinder's search & management API client",
       url='https://github.com/doofinder/pydoof',
@@ -15,9 +16,9 @@ setup(name='PyDoof', version='3.3.5', author='Doofinder',
       long_description=long_description,
       long_description_content_type="text/markdown",
       classifiers=[
-          'Development Status :: 4 - Beta',
+          'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6.7', # On Python >= 3.7 this displays a warning
           'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
       ])
